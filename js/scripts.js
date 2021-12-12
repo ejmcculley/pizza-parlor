90d45c5838cf968b3016f409b0ceaf4fe6e9dd16
Erin McCulley <ejmcculley@gmail.com>
2021-12-12 10:12:56 -0500

    Add pizza selectors and order total elements to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@ function Pizza(toppings, size, price) {
 }
 
 let myPizza = new Pizza(["Soppressata", "Mama Lil's Peppers"], "16 inch", 22);
+
+// let price = priceSum() {
+//   let sum = dropdownInput1 + dropdownInput2 + dropdownInput3 + dropdownInput4;
+//   return "$" + sum;
+// }
+
+// use dropdown selectors to fill toppings, size, and value from selectors get added to priceSum();
