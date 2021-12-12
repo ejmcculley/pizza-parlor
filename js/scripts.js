13a664cc55ab2b9c37d070138cba55bd9b243a4e
Erin McCulley <ejmcculley@gmail.com>
2021-12-12 10:52:31 -0500

    Add UI logic for select menus

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,19 @@ function Pizza(toppings, size, price) {
 
 let myPizza = new Pizza(["Soppressata", "Mama Lil's Peppers"], "16 inch", 22);
 
-// let price = priceSum() {
-//   let sum = dropdownInput1 + dropdownInput2 + dropdownInput3 + dropdownInput4;
-//   return "$" + sum;
-// }
+function priceSum() { 
+  let total = pizzaSize + firstTopping + secondTopping + thirdTopping
+  return total;
+  }
 
-// use dropdown selectors to fill toppings, size, and value from selectors get added to priceSum();
+$(document).ready(function() {
+  $("#orderOnline").onClick(function(event) {
+    event.preventDefault();
+    $("#order").show();
+    $("#home").hide();
+    let pizzaSize = $("#size :selected").val();
+    let firstTopping = $("#toppingOne :selected").val();
+    let secondTopping = $("#toppingTwo :selected").val();
+    let thirdTopping = $("#toppingThree :selected").val();
+  })
+});
