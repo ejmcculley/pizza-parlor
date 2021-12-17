4bd16b01b77188f675614e372690a4ae6d972cc9
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2021-12-17 14:13:27 -0800

    Add updated bootstrap CDN and google fonts to head, update html / css styling, add jquery to hide and show divs

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,31 @@ function Pizza(toppings, size, price) {
 
 let myPizza = new Pizza(["Soppressata", "Mama Lil's Peppers"], "16 inch", 22);
 
-function priceSum() { 
-  let total = pizzaSize + firstTopping + secondTopping + thirdTopping
+function pizzaPrice() { 
+  let total = parseInt(pizzaSizeValue) + parseInt(firstToppingValue) + parseInt(secondToppingValue) + parseInt(thirdToppingValue)
   return total;
   }
 
 $(document).ready(function() {
-  $("#orderOnline").onClick(function(event) {
-    event.preventDefault();
-    $("#order").show();
+  $("#enter").click(function() {
+    $("#pickup").show();
     $("#home").hide();
-    let pizzaSize = $("#size :selected").val();
-    let firstTopping = $("#toppingOne :selected").val();
-    let secondTopping = $("#toppingTwo :selected").val();
-    let thirdTopping = $("#toppingThree :selected").val();
+  });
+  $("#carry-out, #delivery").click(function() {
+    $("#order").show();
+    $("#pickup").hide();
   })
-});
+  $("#addPizza").click(function() {
+    $("#summary").show();
+    $("#order").hide();
+  })
+    let pizzaSize = $("#size option:selected").text();
+    let firstTopping = $("#toppingOne option:selected").text();
+    let secondTopping = $("#toppingTwo option:selected").text();
+    let thirdTopping = $("#toppingThree option:selected").text();
+    let pizzaSizeValue = $("#size").val();
+    let firstToppingValue = $("#toppingOne").val();
+    let secondToppingValue = $("#toppingTwo").val();
+    let thirdToppingValue = $("toppingThree").val();
+  });
+
