8924a71d82bd68a9b5c78e2118e45a8b782e6626
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2021-12-20 21:31:12 -0800

    Remove console logs

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ function Pizza(size, toppings, price) {
 
 Pizza.prototype.pizzaPrice = function(pizzaSizeValue, firstToppingValue, secondToppingValue, thirdToppingValue) { 
   this.price = parseInt(pizzaSizeValue) + parseInt(firstToppingValue) + parseInt(secondToppingValue) + parseInt(thirdToppingValue);
-  console.log(pizzaSizeValue, firstToppingValue, secondToppingValue, thirdToppingValue);
   }
 
 
@@ -36,10 +35,7 @@ $(document).ready(function() {
 
     let myPizza = new Pizza(pizzaSize, [firstTopping, secondTopping, thirdTopping]);
     myPizza.pizzaPrice(pizzaSizeValue, firstToppingValue, secondToppingValue, thirdToppingValue);
-    console.log(myPizza.price);
-    
-    
-
+  
     $("span.myPizzaSummary").html(myPizza.price);
   })  
 });
